109e29e1fda9939b4263779dbe0cdf85e9e161f8
burak-altin <burak.altin@steadfasttech.com.au>
2021-11-06 14:37:43 +1100

    completed

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 2;
+let num2 = 31;
 
-// Write your code here
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 9;
+let num4 = 5;
+let mod = num3 % num4; 
+
+let max = Math.max(1, 2, 3, 4, 20);
